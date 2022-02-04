2d634b759dc8770e325a8e1a837990073ad59a44
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-02-04 08:27:07 +0300

    TypeScript

---
+++ server/app.ts
@@ -0,0 +1,23 @@
+import {default as express, Request, Response} from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.get('/ping', (req: Request, res: Response) => {
+    res.send("Ok)");
+});
+
+module.exports = app;
