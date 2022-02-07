fa399b90a3491d05320d8f028ddba9e1abdc5828
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-02-07 05:57:52 +0300

    Navigation improved

---
+++ adnroid-app/app/src/main/java/com/example/coolstorygame/ui/outer/OuterFragment.java
@@ -0,0 +1,45 @@
+package com.example.coolstorygame.ui.outer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.coolstorygame.databinding.FragmentOuterBinding;
+
+public class OuterFragment extends Fragment {
+
+    private OuterViewModel outerViewModel;
+    private FragmentOuterBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        outerViewModel =
+                new ViewModelProvider(this).get(OuterViewModel.class);
+
+        binding = FragmentOuterBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textOuter;
+        outerViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ adnroid-app/app/src/main/java/com/example/coolstorygame/ui/status/StatusFragment.java
@@ -0,0 +1,45 @@
+package com.example.coolstorygame.ui.status;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.coolstorygame.databinding.FragmentStatusBinding;
+
+public class StatusFragment extends Fragment {
+
+    private StatusViewModel statusViewModel;
+    private FragmentStatusBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        statusViewModel =
+                new ViewModelProvider(this).get(StatusViewModel.class);
+
+        binding = FragmentStatusBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textStatus;
+        statusViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
