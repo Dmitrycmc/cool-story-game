d99ca6c9ef9234beff19d421174c41f9b14fcad3
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2022-03-31 23:17:03 +0300

    WS fix

--- web-app/src/app/services/players.service.ts
+++ web-app/src/app/services/players.service.ts
@@ -10,7 +10,7 @@ export class PlayersService {
   constructor() { }
 
   public openWebSocket(roomId: string, toggleButtonEvent?: Observable<any>): Observable<any> {
-    const ws = webSocket("ws://localhost:4200/api/web-socket");
+    const ws = webSocket(`ws://${window.location.host}/api/web-socket`);
     toggleButtonEvent?.subscribe((msg: any) => {
       ws.next({
         ...msg,
